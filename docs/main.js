ce89004ade693004808f94c0ddbe1cf3fcc85170
SamhitaVinnakota <samhita@emproto.com>
2022-01-28 18:23:37 +0530

    modified docs

--- docs/main.js
+++ docs/main.js
@@ -1311,7 +1311,7 @@ Object(_angular_platform_browser_dynamic__WEBPACK_IMPORTED_MODULE_1__["platformB
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! D:\factoreal\github\src\main.ts */"./src/main.ts");
+module.exports = __webpack_require__(/*! D:\factoreal\spa\src\main.ts */"./src/main.ts");
 
 
 /***/ })
--- docs/styles.js
+++ docs/styles.js
@@ -598,8 +598,8 @@ if(false) {}
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! D:\factoreal\github\src\styles.css */"./src/styles.css");
-module.exports = __webpack_require__(/*! D:\factoreal\github\node_modules\bootstrap\dist\css\bootstrap.min.css */"./node_modules/bootstrap/dist/css/bootstrap.min.css");
+__webpack_require__(/*! D:\factoreal\spa\src\styles.css */"./src/styles.css");
+module.exports = __webpack_require__(/*! D:\factoreal\spa\node_modules\bootstrap\dist\css\bootstrap.min.css */"./node_modules/bootstrap/dist/css/bootstrap.min.css");
 
 
 /***/ })
